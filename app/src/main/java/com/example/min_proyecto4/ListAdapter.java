dbe9719dc5fb3e7e678daad0e43986085ab66cec
JOSE MADRIGAL <jmadrigal@cecropiasolutions.com>
2020-02-09 15:55:10 -0600

    Se añade funcionalidad a los botones de archivar y eliminar

--- app/src/main/java/com/example/min_proyecto4/ListAdapter.java
+++ app/src/main/java/com/example/min_proyecto4/ListAdapter.java
@@ -1,14 +1,15 @@
 package com.example.min_proyecto4;
 
 import android.content.Context;
-import android.speech.tts.TextToSpeech;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.Console;
 import java.util.List;
 
 public class ListAdapter extends BaseAdapter {
@@ -41,11 +42,30 @@ public class ListAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater layoutPopulator = LayoutInflater.from(this.listAdapter);
-        View viewToCreate = layoutPopulator.inflate(R.layout.reminders_list, null);
+        View viewToRender = layoutPopulator.inflate(R.layout.reminders_list, null);
+        setElementsEvents(viewToRender);
         String clickedReminder = remindersList.get(position);
-        TextView textView = viewToCreate.findViewById(R.id.textView);
+        TextView textView = viewToRender.findViewById(R.id.reminderText);
         textView.setText(clickedReminder);
 
-        return viewToCreate;
+        return viewToRender;
+    }
+
+    public void setElementsEvents(View viewToRender) {
+        ImageButton deleteButton = viewToRender.findViewById(R.id.deleteButton);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("I was clicked");
+            }
+        });
+
+        ImageButton archiveButton = viewToRender.findViewById(R.id.archiveButton);
+        archiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Archive was clicked");
+            }
+        });
     }
 }
