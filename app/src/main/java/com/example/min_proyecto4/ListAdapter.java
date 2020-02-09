da24fdd9a0ae5296799309b7a971e5965464fb2f
JOSE MADRIGAL <jmadrigal@cecropiasolutions.com>
2020-02-09 15:21:55 -0600

    Trabajando en la pantalla de lista de reminders

--- app/src/main/java/com/example/min_proyecto4/ListAdapter.java
+++ app/src/main/java/com/example/min_proyecto4/ListAdapter.java
@@ -1,10 +1,12 @@
 package com.example.min_proyecto4;
 
 import android.content.Context;
+import android.speech.tts.TextToSpeech;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.List;
--- app/src/main/java/com/example/min_proyecto4/RemindersActivity.java
+++ app/src/main/java/com/example/min_proyecto4/RemindersActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
