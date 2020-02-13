ef6bc50c77152dc9705b6ff7a74cfd6744f99a04
JOSE MADRIGAL <jmadrigal@cecropiasolutions.com>
2020-02-13 04:26:02 -0600

    Trabajando en las llamadas a las lambdas de AWS

--- app/src/main/java/com/example/min_proyecto4/MainActivity.java
+++ app/src/main/java/com/example/min_proyecto4/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.min_proyecto4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] urls = new String[0];
+        new test().execute(urls);
 
         Intent remindersActivity = new Intent(MainActivity.this, RemindersActivity.class);
         startActivity(remindersActivity);
