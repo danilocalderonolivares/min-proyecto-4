325a40527d93d8644dcdf78d2e08d63107368563
danilocalderonolivares <dcalderono@ucenfotec.ac.cr>
2020-02-18 19:46:05 -0600

    Se continua trabajando en inicio de sesion

--- app/src/main/java/com/example/min_proyecto4/MainActivity.java
+++ app/src/main/java/com/example/min_proyecto4/MainActivity.java
@@ -2,18 +2,11 @@ package com.example.min_proyecto4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-    EditText mTextUsername;
-    EditText mTextPassword;
-    Button mButtonLoging;
-    TextView textViewSignup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        mTextUsername = (EditText)findViewById(R.id.edittext_username);
-        mTextPassword = (EditText)findViewById(R.id.edittext_password);
-        mButtonLoging = (Button)findViewById(R.id.edittext_login);
-        textViewSignup = (TextView)findViewById(R.id.textview_signup);
-        textViewSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent signupIntent =  new Intent(MainActivity.this, signup.class);
-                startActivity(signupIntent);
-            }
-        });
 
 
     }
