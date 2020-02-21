89c5a8204fa01ec0cab9b28411636f4aef42d938
danilocalderonolivares <dcalderono@ucenfotec.ac.cr>
2020-02-20 22:16:15 -0600

    Se logro conectar la aplicacion correctamene con firebase, tambien se agrego la funcionalidad correctamente

--- app/src/main/java/com/example/min_proyecto4/MainActivity.java
+++ app/src/main/java/com/example/min_proyecto4/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.min_proyecto4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,31 +9,60 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    EditText mTextUsername;
-    EditText mTextPassword;
-    Button mButtonLoging;
-    TextView textViewSignup;
-
+    private EditText edtEmail, edtUserName, edtPassword;
+    private Button btnSignUp, btnSignIn;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Inputs
+        edtEmail = findViewById(R.id.idEmail);
+        edtUserName = findViewById(R.id.idUserName);
+        edtPassword = findViewById(R.id.idPassword);
+        // botones
+        btnSignIn = findViewById(R.id.idSignIn);
+        btnSignUp = findViewById(R.id.idSignUp);
 
-
-        mTextUsername = (EditText)findViewById(R.id.edittext_username);
-        mTextPassword = (EditText)findViewById(R.id.edittext_password);
-        mButtonLoging = (Button)findViewById(R.id.edittext_login);
-        textViewSignup = (TextView)findViewById(R.id.textview_signup);
-        textViewSignup.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent signupIntent =  new Intent(MainActivity.this, signup.class);
-                startActivity(signupIntent);
+            public void onClick(View view) {
+                signUp();
             }
         });
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            //siguiente actividad
+        }
+    }
+    private void signUp() {
+        mAuth.createUserWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Autenticación Correcta", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Falló de autenticación", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
 }
