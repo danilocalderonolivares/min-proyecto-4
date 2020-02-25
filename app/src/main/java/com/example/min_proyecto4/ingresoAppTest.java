02af7878c167b597e9138d33066246eb8e41fb97
danilocalderonolivares <dcalderono@ucenfotec.ac.cr>
2020-02-24 21:54:05 -0600

    Se integro la funcionalidad de salir

--- app/src/main/java/com/example/min_proyecto4/ingresoAppTest.java
+++ app/src/main/java/com/example/min_proyecto4/ingresoAppTest.java
@@ -2,13 +2,33 @@ package com.example.min_proyecto4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ingresoAppTest extends AppCompatActivity {
+    private Button signOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso_app_test);
+        signOutButton = findViewById(R.id.idSignOut);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+    }
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        Toast.makeText(this, "Se cerro correctamemte la sesopm",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
